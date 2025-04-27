790debfc8daa2557662f66d900b9d760a6abd4d1
bukkybyte <bukola1030@gmail.com>
2025-04-27 19:03:29 +0100

    added navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import { ModeToggle } from "@/components/ModeToggle";
 import { Button } from "@/components/ui/button";
 import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="m-4">
       <SignedOut>
         <SignInButton mode="modal">
         <Button>
@@ -16,6 +17,9 @@ export default function Home() {
       <SignedIn>
         <UserButton />
       </SignedIn>
+
+      <Button variant={"secondary"}> Click Me</Button>
+      <ModeToggle/>
     </div>
   );
 }
